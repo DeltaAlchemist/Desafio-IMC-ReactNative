f6c49d235858d713b6cee02522581dcc96438013
Logon Aluno <logonrmlocal@fiap.com.br>
2022-04-07 11:11:13 -0300

    refactor: desafio by prof

--- App.js
+++ App.js
@@ -1,20 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View, StatusBar, Alert, TextInput, Button } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      peso: null,
+      altura: null,
+    }   
+  }
+
+  calcularImc = _ => {
+    const { peso, altura } = this.state
+
+    const pesoNum = parseFloat(peso)
+    const alturaNum = parseFloat(altura)
+    if (Number.isNaN(pesoNum) || Number.isNaN(alturaNum)) {
+      Alert.alert("Erro", "Preencha os campos corretamente.")
+      return
+    }
+
+    const imc = (peso / ((altura / 100) * (altura / 100)))
+
+    const categoria = ""
+
+    if (imc < 18.5) {
+      categoria = "Abaixo do peso"
+    } else if (imc < 25) {
+      categoria = "Peso Normal"
+    } else if (imc < 30) {
+      categoria = "Acima do Peso"
+    } else {
+      categoria = "Obesidade"
+    }
+    
+    Alert.alert(`Seu IMC é ${imc.toFixed(1)}`, `Sua Categoria é ${categoria}.`)
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          placeholder="Peso"
+          onChangeText={peso => this.setState({ peso: peso })}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Altura"
+          onChangeText={altura => this.setState({ altura: altura })}
+        />
+        <Button 
+          color='#19F'
+          title='Calcular'
+          onPress={this.calcularImc}
+        />
+        <StatusBar />
+      </View>
+    );
+  }
+
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 8
   },
-});
+  input: {
+    borderWidth: 0.5,
+    paddingHorizontal: 5,
+    marginBottom: 15,
+    fontSize: 16
+  }
+})
